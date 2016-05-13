0ceafefc5df7fe7f98fb90d39ee73c7aac3da533
Pablo Bejarano <pabhoz@gmail.com>
2016-05-12 19:01:35 -0500

    Delegado de Negocio Base Listo

--- src/main/java/co/edu/usbcali/demo/delegado/DelegadoDeNegocio.java
+++ src/main/java/co/edu/usbcali/demo/delegado/DelegadoDeNegocio.java
@@ -7,9 +7,21 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import co.edu.usbcali.demo.logica.IClienteLogica;
+import co.edu.usbcali.demo.logica.IConsignacionesLogica;
+import co.edu.usbcali.demo.logica.ICuentasLogica;
+import co.edu.usbcali.demo.logica.IRetirosLogica;
 import co.edu.usbcali.demo.logica.ITiposDocumentosLogica;
+import co.edu.usbcali.demo.logica.ITiposUsuariosLogica;
+import co.edu.usbcali.demo.logica.IUsuariosLogica;
 import co.edu.usbcali.demo.modelo.Clientes;
+import co.edu.usbcali.demo.modelo.Consignaciones;
+import co.edu.usbcali.demo.modelo.ConsignacionesId;
+import co.edu.usbcali.demo.modelo.Cuentas;
+import co.edu.usbcali.demo.modelo.Retiros;
+import co.edu.usbcali.demo.modelo.RetirosId;
 import co.edu.usbcali.demo.modelo.TiposDocumentos;
+import co.edu.usbcali.demo.modelo.TiposUsuarios;
+import co.edu.usbcali.demo.modelo.Usuarios;
 
 @Scope("singleton")
 @Component("delegadoDeNegocio")
@@ -17,9 +29,24 @@ public class DelegadoDeNegocio implements IDelegadoDeNegocio {
 	
 	@Autowired
 	private IClienteLogica clienteLogica;
+
+	@Autowired
+	private IConsignacionesLogica consignacionesLogica;
+
+	@Autowired
+	private ICuentasLogica cuentasLogica;
+
+	@Autowired
+	private IRetirosLogica retirosLogica;
 	
 	@Autowired
 	private ITiposDocumentosLogica tiposDocumentosLogica;
+
+	@Autowired
+	private ITiposUsuariosLogica tiposUsuariosLogica;
+
+	@Autowired
+	private IUsuariosLogica usuariosLogica;
 
 	@Override
 	public void grabarClientes(Clientes clientes) throws Exception {
@@ -74,6 +101,146 @@ public class DelegadoDeNegocio implements IDelegadoDeNegocio {
 	public List<TiposDocumentos> consultarTodosTiposDocumentos() throws Exception {
 		
 		return tiposDocumentosLogica.consultarTodos();
+	}
+
+	@Override
+	public void grabarConsignaciones(Consignaciones consignaciones) throws Exception {
+		consignacionesLogica.grabar(consignaciones);
+		
+	}
+
+	@Override
+	public void modificarConsignaciones(Consignaciones consignaciones) throws Exception {
+		consignacionesLogica.modificar(consignaciones);
+		
+	}
+
+	@Override
+	public void borrarConsignaciones(Consignaciones consignaciones) throws Exception {
+		consignacionesLogica.borrar(consignaciones);
+		
+	}
+
+	@Override
+	public Consignaciones consultarConsignacionesPorId(ConsignacionesId consignacionesId) throws Exception {
+		return consignacionesLogica.consultarPorId(consignacionesId);
+	}
+
+	@Override
+	public List<Consignaciones> consultarTodosConsignaciones() throws Exception {
+		return consignacionesLogica.consultarTodos();
+	}
+
+	@Override
+	public void grabarCuentas(Cuentas cuentas) throws Exception {
+		cuentasLogica.grabar(cuentas);
+		
+	}
+
+	@Override
+	public void modificarCuentas(Cuentas cuentas) throws Exception {
+		cuentasLogica.modificar(cuentas);
+		
+	}
+
+	@Override
+	public void borrarCuentas(Cuentas cuentas) throws Exception {
+		cuentasLogica.borrar(cuentas);
+		
+	}
+
+	@Override
+	public Cuentas consultarCuentasPorId(String cuentasId) throws Exception {
+		return cuentasLogica.consultarPorId(cuentasId);
+	}
+
+	@Override
+	public List<Cuentas> consultarTodosCuentas() throws Exception {
+		return cuentasLogica.consultarTodos();
+	}
+
+	@Override
+	public void grabarRetiros(Retiros retiros) throws Exception {
+		retirosLogica.grabar(retiros);
+		
+	}
+
+	@Override
+	public void modificarRetiros(Retiros retiros) throws Exception {
+		retirosLogica.modificar(retiros);
+		
+	}
+
+	@Override
+	public void borrarRetiros(Retiros retiros) throws Exception {
+		retirosLogica.borrar(retiros);
+		
+	}
+
+	@Override
+	public Retiros consultarRetirosPorId(RetirosId retirosId) throws Exception {
+		return retirosLogica.consultarPorId(retirosId);
+	}
+
+	@Override
+	public List<Retiros> consultarTodosRetiros() throws Exception {
+		return retirosLogica.consultarTodos();
+	}
+
+	@Override
+	public void grabarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception {
+		tiposUsuariosLogica.grabar(tiposUsuarios);
+		
+	}
+
+	@Override
+	public void modificarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception {
+		tiposUsuariosLogica.modificar(tiposUsuarios);
+		
+	}
+
+	@Override
+	public void borrarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception {
+		tiposUsuariosLogica.borrar(tiposUsuarios);
+		
+	}
+
+	@Override
+	public TiposUsuarios consultarTiposUsuariosPorId(Long tiposUsuariosId) throws Exception {
+		return tiposUsuariosLogica.consultarPorId(tiposUsuariosId);
+	}
+
+	@Override
+	public List<TiposUsuarios> consultarTodosTiposUsuarios() throws Exception {
+		return tiposUsuariosLogica.consultarTodos();
+	}
+
+	@Override
+	public void grabarUsuarios(Usuarios usuarios) throws Exception {
+		usuariosLogica.grabar(usuarios);
+		
+	}
+
+	@Override
+	public void modificarUsuarios(Usuarios usuarios) throws Exception {
+		usuariosLogica.modificar(usuarios);
+		
+	}
+
+	@Override
+	public void borrarUsuarios(Usuarios usuarios) throws Exception {
+		usuariosLogica.borrar(usuarios);
+		
+	}
+
+	@Override
+	public Usuarios consultarUsuariosPorId(long usuariosId) throws Exception {
+		return usuariosLogica.consultarPorId(usuariosId);
+	}
+
+	@Override
+	public List<Usuarios> consultarTodosUsuarios() throws Exception {
+		return usuariosLogica.consultarTodos();
 	}
 
 }
--- src/main/java/co/edu/usbcali/demo/delegado/IDelegadoDeNegocio.java
+++ src/main/java/co/edu/usbcali/demo/delegado/IDelegadoDeNegocio.java
@@ -3,7 +3,14 @@ package co.edu.usbcali.demo.delegado;
 import java.util.List;
 
 import co.edu.usbcali.demo.modelo.Clientes;
+import co.edu.usbcali.demo.modelo.Consignaciones;
+import co.edu.usbcali.demo.modelo.ConsignacionesId;
+import co.edu.usbcali.demo.modelo.Cuentas;
+import co.edu.usbcali.demo.modelo.Retiros;
+import co.edu.usbcali.demo.modelo.RetirosId;
 import co.edu.usbcali.demo.modelo.TiposDocumentos;
+import co.edu.usbcali.demo.modelo.TiposUsuarios;
+import co.edu.usbcali.demo.modelo.Usuarios;
 
 public interface IDelegadoDeNegocio {
 	
@@ -13,11 +20,40 @@ public interface IDelegadoDeNegocio {
 	public Clientes consultarClinetePorId(long cliId)throws Exception;
 	public List<Clientes> consultarTodosClientes()throws Exception;
 	
+	public void grabarConsignaciones(Consignaciones consignaciones) throws Exception;
+	public void modificarConsignaciones(Consignaciones consignaciones) throws Exception;
+	public void borrarConsignaciones(Consignaciones consignaciones) throws Exception;
+	public Consignaciones consultarConsignacionesPorId(ConsignacionesId consignacionesId) throws Exception;
+	public List<Consignaciones> consultarTodosConsignaciones() throws Exception;
 	
+	public void grabarCuentas(Cuentas cuentas) throws Exception;
+	public void modificarCuentas(Cuentas cuentas) throws Exception;
+	public void borrarCuentas(Cuentas cuentas) throws Exception;
+	public Cuentas consultarCuentasPorId(String id) throws Exception;
+	public List<Cuentas> consultarTodosCuentas() throws Exception;
+
+	public void grabarRetiros(Retiros retiros) throws Exception;
+	public void modificarRetiros(Retiros retiros) throws Exception;
+	public void borrarRetiros(Retiros retiros) throws Exception;
+	public Retiros consultarRetirosPorId(RetirosId retirosId) throws Exception;
+	public List<Retiros> consultarTodosRetiros() throws Exception;
+
 	public void grabarTiposDocumentos(TiposDocumentos entity)throws  Exception;
 	public void modificarTiposDocumentos(TiposDocumentos entity)throws  Exception;
 	public void borrarTiposDocumentos(TiposDocumentos entity)throws  Exception;
 	public TiposDocumentos consultarTipoDocumentoId(long tdocCodigo)throws  Exception;
 	public List<TiposDocumentos> consultarTodosTiposDocumentos()throws Exception;
+
+	public void grabarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception;
+	public void modificarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception;
+	public void borrarTiposUsuarios(TiposUsuarios tiposUsuarios) throws Exception;
+	public TiposUsuarios consultarTiposUsuariosPorId(Long id) throws Exception;
+	public List<TiposUsuarios> consultarTodosTiposUsuarios() throws Exception;
+
+	public void grabarUsuarios(Usuarios usuarios) throws Exception;
+	public void modificarUsuarios(Usuarios usuarios) throws Exception;
+	public void borrarUsuarios(Usuarios usuarios) throws Exception;
+	public Usuarios consultarUsuariosPorId(long id) throws Exception;
+	public List<Usuarios> consultarTodosUsuarios() throws Exception;
 
 }
